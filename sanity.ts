77bc6c02bcfc716c9f1fa3c51cbc9c89c68cdde2
mohamed0807 <122071103+mohamed0807@users.noreply.github.com>
2023-01-24 09:19:17 +0530

    Update sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -3,8 +3,8 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_DATASET || "production",
-  //   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  projectId: "391ox5ih",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  // projectId: "391ox5ih",
   apiVersion: "2021-10-21",
   //   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
